6fa9ff9effb213fbed57235839bc94353f0bb3c5
Emiliano Sota <esota@compartamos.com>
2023-07-20 18:23:35 -0600

    se agrego un comentario

--- src/app/Heros/list.module.ts
+++ src/app/Heros/list.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
 import { ListComponent } from './list/list.component';
 import { CommonModule } from '@angular/common';
-
+//hola mundo
 @NgModule({
   exports: [
     HeroComponent,
